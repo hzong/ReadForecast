6ae80a192fbc0f1a974af579f8ef7cc3257bf522
hzong <917481540@qq.com>
2015-06-13 20:27:32 +0800

    First commit.

---
+++ src/com/example/readforecast/activity/ForecastMainActivity.java
@@ -0,0 +1,23 @@
+package com.example.readforecast.activity;
+
+import com.example.android.base.BaseActivity;
+import com.example.readforecast.R;
+
+import android.os.Bundle;
+
+
+/** 
+* @className  ForecastMainActivity 
+* @describe  读预报主活动
+* @author  hzong
+* @datetime  2015-6-13 下午5:56:56  
+*/ 
+public class ForecastMainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast_main);
+    }
+
+}
